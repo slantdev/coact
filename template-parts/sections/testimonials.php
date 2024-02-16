ab18994a1f30c0e135f0f3a5a7511662a42c8ddf
Slant Dev <build@amazeworx.com>
2024-02-17 00:32:09 +0700

    testimonials

--- template-parts/sections/testimonials.php
+++ template-parts/sections/testimonials.php
@@ -14,7 +14,9 @@ $testimonials = get_sub_field('testimonials');
 $intro = $testimonials['intro'];
 $headline = $intro['headline'];
 $description = $intro['description'];
+$dynamic_custom = $testimonials['dynamic_custom'];
 $choose_testimonial = $testimonials['choose_testimonial'];
+$select_testimonial_categories = $testimonials['select_testimonial_categories'];
 $testimonials_id = uniqid('testimonials-');
 ?>
 
@@ -36,33 +38,96 @@ $testimonials_id = uniqid('testimonials-');
             <div class="mt-6 text-lg font-medium"><?php echo $description ?></div>
           <?php endif; ?>
         </div>
-        <div id="<?php echo $testimonials_id ?>" class="testimonial-swiper relative pb-20">
-          <div class="swiper container max-w-screen-lg mx-auto">
-            <div class="swiper-wrapper">
-              <?php foreach ($choose_testimonial as $testimony) : ?>
+        <?php if ($dynamic_custom == 'custom' && $choose_testimonial) : ?>
+          <div id="<?php echo $testimonials_id ?>" class="testimonial-swiper relative pb-20">
+            <div class="swiper container max-w-screen-lg mx-auto">
+              <div class="swiper-wrapper">
+                <?php foreach ($choose_testimonial as $testimony) : ?>
+                  <?php
+                  $testimonial_title = get_the_title($testimony->ID);
+                  $testimonial_text = get_the_content('null', false, $testimony->ID);
+                  ?>
+                  <div class="swiper-slide text-center">
+                    <?php if ($testimonial_text) {
+                      echo '<div class="text-brand-purple text-3xl font-bold" style="color: var(--section-link-color)">' . $testimonial_text . '</div>';
+                    } ?>
+                    <?php if ($testimonial_title) {
+                      echo '<div class="mt-6 text-lg font-bold">' . $testimonial_title . '</div>';
+                    } ?>
+                  </div>
+                <?php endforeach; ?>
+              </div>
+            </div>
+            <div class="absolute left-0 -bottom-4 right-0">
+              <div class="container max-w-screen-3xl mx-auto">
+                <div class="swiper-pagination text-center relative [&>.swiper-pagination-bullet]:rounded-full" style="--swiper-pagination-bullet-height:12px;--swiper-pagination-bullet-width:12px;--swiper-pagination-bullet-inactive-color:#E2E2E2;--swiper-pagination-bullet-horizontal-gap:6px;--swiper-theme-color:var(--section-link-color);--swiper-pagination-bullet-inactive-opacity:1;"></div>
+              </div>
+            </div>
+            <div class="swiper-button-prev left-0 lg:left-8 after:content-['prev'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full"></div>
+            <div class="swiper-button-next right-0 lg:right-8 after:content-['next'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full"></div>
+          </div>
+        <?php endif; ?>
+        <?php if ($dynamic_custom == 'dynamic' && $select_testimonial_categories) : ?>
+          <div id="<?php echo $testimonials_id ?>" class="testimonial-swiper relative pb-20">
+            <div class="swiper container max-w-screen-lg mx-auto">
+              <div class="swiper-wrapper">
+
                 <?php
-                $testimonial_title = get_the_title($testimony->ID);
-                $testimonial_text = get_the_content('null', false, $testimony->ID);
+                $term_ids = $select_testimonial_categories;
+
+                $args = array(
+                  'post_type'      => 'testimonial',
+                  'posts_per_page' => -1,
+                  'orderby'        => 'date',
+                  'order'          => 'DESC',
+                  'tax_query'      => array(
+                    array(
+                      'taxonomy' => 'testimonial-category',
+                      'field'    => 'term_id',
+                      'terms'    => $term_ids,
+                      'operator' => 'IN',
+                    ),
+                  ),
+                );
+
+                $query = new WP_Query($args);
+
+                if ($query->have_posts()) {
+                  while ($query->have_posts()) {
+                    $query->the_post();
                 ?>
-                <div class="swiper-slide text-center">
-                  <?php if ($testimonial_text) {
-                    echo '<div class="text-brand-purple text-3xl font-bold">' . $testimonial_text . '</div>';
-                  } ?>
-                  <?php if ($testimonial_title) {
-                    echo '<div class="mt-6 text-lg font-bold">' . $testimonial_title . '</div>';
-                  } ?>
-                </div>
-              <?php endforeach; ?>
+                    <?php
+                    $testimonial_id = get_the_ID();
+                    $testimonial_title = get_the_title($testimonial_id);
+                    $testimonial_text = get_the_content('null', false, $testimonial_id);
+                    ?>
+                    <div class="swiper-slide text-center">
+                      <?php if ($testimonial_text) {
+                        echo '<div class="text-brand-purple text-3xl font-bold" style="color: var(--section-link-color)">' . $testimonial_text . '</div>';
+                      } ?>
+                      <?php if ($testimonial_title) {
+                        echo '<div class="mt-6 text-lg font-bold">' . $testimonial_title . '</div>';
+                      } ?>
+                    </div>
+                <?php
+                  }
+                  wp_reset_postdata();
+                } else {
+                  // If no posts found
+                  echo 'No testimonials found.';
+                }
+                ?>
+              </div>
             </div>
-          </div>
-          <div class="absolute left-0 -bottom-4 right-0">
-            <div class="container max-w-screen-3xl mx-auto">
-              <div class="swiper-pagination text-center relative [&>.swiper-pagination-bullet]:rounded-full" style="--swiper-pagination-bullet-height:12px;--swiper-pagination-bullet-width:12px;--swiper-pagination-bullet-inactive-color:#E2E2E2;--swiper-pagination-bullet-horizontal-gap:6px;--swiper-theme-color:#B2519E;--swiper-pagination-bullet-inactive-opacity:1;"></div>
+            <div class="absolute left-0 -bottom-4 right-0">
+              <div class="container max-w-screen-3xl mx-auto">
+                <div class="swiper-pagination text-center relative [&>.swiper-pagination-bullet]:rounded-full" style="--swiper-pagination-bullet-height:12px;--swiper-pagination-bullet-width:12px;--swiper-pagination-bullet-inactive-color:#E2E2E2;--swiper-pagination-bullet-horizontal-gap:6px;--swiper-theme-color:var(--section-link-color);--swiper-pagination-bullet-inactive-opacity:1;"></div>
+              </div>
             </div>
+            <div class="swiper-button-prev left-0 lg:left-8 after:content-['prev'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full" style="color: var(--section-link-color)"></div>
+            <div class="swiper-button-next right-0 lg:right-8 after:content-['next'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full" style="color: var(--section-link-color)"></div>
           </div>
-          <div class="swiper-button-prev left-0 lg:left-8 after:content-['prev'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full"></div>
-          <div class="swiper-button-next right-0 lg:right-8 after:content-['next'] after:text-lg after:lg:text-4xl text-brand-purple font-bold -mt-12 -translate-y-full"></div>
-        </div>
+        <?php endif; ?>
         <script>
           new Swiper('#<?php echo $testimonials_id ?> .swiper', {
             slidesPerView: 1,
