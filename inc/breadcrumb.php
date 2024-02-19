6e84c4d2a23325ef95c501ae8aa154b89280209d
Slant Dev <build@amazeworx.com>
2024-02-19 12:07:18 +0700

    job

--- inc/breadcrumb.php
+++ inc/breadcrumb.php
@@ -4,29 +4,46 @@
  * https://gist.github.com/amboutwe/ea0791e184668a5c7bd7bbe357c598e9
  */
 
-//add_filter('wpseo_breadcrumb_links', 'coact_yoast_seo_breadcrumb_append_link');
+add_filter('wpseo_breadcrumb_links', 'coact_yoast_seo_breadcrumb_append_link');
 function coact_yoast_seo_breadcrumb_append_link($links)
 {
   global $post;
 
-  if (is_singular('services')) {
-    $breadcrumb[] = array(
-      'url' => site_url('/services/'),
-      'text' => 'Services',
-    );
-    array_splice($links, 1, -2, $breadcrumb);
-  } elseif (is_tax('industry')) {
-    $breadcrumb[] = array(
-      'url' => site_url('/industry/'),
-      'text' => 'Industry',
-    );
-    array_splice($links, 1, -2, $breadcrumb);
+  $cpt_jobs_settings = get_field('cpt_jobs_settings', 'option');
+  $job_parent_page = $cpt_jobs_settings['parent_page'];
+
+  if (is_singular('job')) {
+    if ($job_parent_page) {
+      $permalink = get_permalink($job_parent_page->ID);
+      $title = $job_parent_page->post_title;
+      $breadcrumb[] = array(
+        'url' => $permalink,
+        'text' => $title,
+      );
+      array_splice($links, 1, -2, $breadcrumb);
+    }
   }
+
+  // if (is_singular('services')) {
+  //   $breadcrumb[] = array(
+  //     'url' => site_url('/services/'),
+  //     'text' => 'Services',
+  //   );
+  //   array_splice($links, 1, -2, $breadcrumb);
+  // } elseif (is_tax('industry')) {
+  //   $breadcrumb[] = array(
+  //     'url' => site_url('/industry/'),
+  //     'text' => 'Industry',
+  //   );
+  //   array_splice($links, 1, -2, $breadcrumb);
+  // }
 
   return $links;
 }
 
 
-add_filter('wpseo_breadcrumb_separator', function ($separator) {
+add_filter('wpseo_breadcrumb_separator', 'coact_wpseo_breadcrumb_separator');
+function coact_wpseo_breadcrumb_separator($separator)
+{
   return '<span class="inline-block px-2"><svg class="inline" xmlns="http://www.w3.org/2000/svg" width="9.245" height="15.38" viewBox="0 0 9.245 15.38"><path id="Arrow_2" data-name="Arrow 2" d="M1846.773,982.341a.677.677,0,0,1,.469.188l6.812,6.505a.687.687,0,0,1,0,.993l-6.812,6.505a.679.679,0,0,1-.963-.025.688.688,0,0,1,.024-.968l6.292-6.009-6.292-6.009a.688.688,0,0,1-.024-.968A.678.678,0,0,1,1846.773,982.341Z" transform="translate(-1845.52 -981.841)" fill="#f26b49" stroke="#f26b49" stroke-width="1" /></svg></span>';
-});
+}
--- template-parts/single/single-job.php
+++ template-parts/single/single-job.php
@@ -1,55 +1,49 @@
 <?php
 
 $enable_page_header = true;
+$text_color = '#000000';
+$text_background_color = 'rgba(242,107,73,0.93)';
+$background_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
+$background_position = 'center';
+$bg_image_class = '';
+if ($background_position) {
+  $bg_image_class = ' object-' . $background_position;
+}
+$title = get_the_title();
+$text_style = $text_color ? 'color: ' . $text_color . ';' : '';
+$text_bg_style = $text_background_color ? 'background-color: ' . $text_background_color . ';' : '';
 
-if ($enable_page_header) :
-  $text_color = '#000000';
-  $text_background_color = 'rgba(242,107,73,0.93)';
-  $background_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
-  $background_position = 'center';
-  $bg_image_class = '';
-  if ($background_position) {
-    $bg_image_class = ' object-' . $background_position;
-  }
-
-  $title = get_the_title();
-
-  $text_style = $text_color ? 'color: ' . $text_color . ';' : '';
-
-  $text_bg_style = $text_background_color ? 'background-color: ' . $text_background_color . ';' : '';
-
-  $enable_breadcrumbs = true;
+$enable_breadcrumbs = true;
 
 ?>
-  <section class="bg-gradient-to-b from-brand-light-gray from-90% via-white via-90% to-white">
-    <div class="max-w-screen-5xl px-4 mx-auto 4xl:px-8">
-      <div class="aspect-w-16 aspect-h-5 rounded-3xl overflow-hidden">
-        <div class="bg-zinc-300 w-full h-full">
-          <?php if ($background_image) : ?>
-            <img class="object-cover w-full h-full <?php echo $bg_image_class ?>" src="<?php echo $background_image; ?>" alt="">
-          <?php endif; ?>
-          <div class="absolute top-0 left-0 w-[1300px] h-[1300px] rounded-full bg-brand-orange bg-opacity-80 -translate-y-1/4 -translate-x-1/4" style="<?php echo $text_bg_style ?>"></div>
-          <div class="absolute inset-0 flex items-center p-12">
-            <div class="container max-w-screen-xxl mx-auto">
-              <div class="max-w-lg font-montserrat" style="<?php echo $text_style ?>">
-                <?php if ($enable_breadcrumbs) : ?>
-                  <?php
-                  if (function_exists('yoast_breadcrumb')) {
-                    yoast_breadcrumb('<div class="breadcrumbs mb-10 text-white">', '</div>');
-                  }
-                  ?>
-                <?php endif; ?>
-                <?php if ($title) : ?>
-                  <h2 class="text-4xl text-white font-bold mb-4"><?php echo $title ?></h2>
-                <?php endif; ?>
-              </div>
+<section class="bg-gradient-to-b from-brand-light-gray from-90% via-white via-90% to-white">
+  <div class="max-w-screen-5xl px-4 mx-auto 4xl:px-8">
+    <div class="aspect-w-16 aspect-h-5 rounded-3xl overflow-hidden">
+      <div class="bg-zinc-300 w-full h-full">
+        <?php if ($background_image) : ?>
+          <img class="object-cover w-full h-full <?php echo $bg_image_class ?>" src="<?php echo $background_image; ?>" alt="">
+        <?php endif; ?>
+        <div class="absolute top-0 left-0 w-[1300px] h-[1300px] rounded-full bg-brand-orange bg-opacity-80 -translate-y-1/4 -translate-x-1/4" style="<?php echo $text_bg_style ?>"></div>
+        <div class="absolute inset-0 flex items-center p-12">
+          <div class="container max-w-screen-xxl mx-auto">
+            <div class="max-w-lg font-montserrat" style="<?php echo $text_style ?>">
+              <?php if ($enable_breadcrumbs) : ?>
+                <?php
+                if (function_exists('yoast_breadcrumb')) {
+                  yoast_breadcrumb('<div class="breadcrumbs mb-10 text-white">', '</div>');
+                }
+                ?>
+              <?php endif; ?>
+              <?php if ($title) : ?>
+                <h2 class="text-4xl text-white font-bold mb-4"><?php echo $title ?></h2>
+              <?php endif; ?>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </section>
-<?php endif; ?>
+  </div>
+</section>
 
 <section class="bg-white">
   <div class="relative container max-w-screen-xxl mx-auto pt-12 lg:pt-20 xl:pt-20 pb-12 lg:pb-20 xl:pb-36">
@@ -60,6 +54,17 @@ if ($enable_page_header) :
         </div>
         <div class="prose max-w-none xl:prose-lg mr-auto text-left mb-6 xl:mb-8">
           <?php the_content() ?>
+          <?php
+          $job_settings = get_field('job_settings');
+          $url = isset($job_settings['external_link']['url']) ? $job_settings['external_link']['url'] : '';
+          $title = isset($job_settings['external_link']['title']) ? $job_settings['external_link']['title'] : 'Apply';
+          $target = isset($job_settings['external_link']['target']) ? $job_settings['external_link']['target'] : '_self';
+          if ($url) {
+            echo '<div class="mt-6 xl:mt-8">';
+            echo '<a href="' . $url . '" target="' . $target . '" class="inline-block rounded-full font-poppins font-semibold px-6 py-2 text-sm lg:text-xl lg:px-10 lg:py-4 bg-brand-orange text-white border border-transparent no-underline shadow-md hover:shadow-lg transition-all duration-200">' . $title . '</a>';
+            echo '</div>';
+          }
+          ?>
         </div>
       </div>
       <div class="w-full order-2 lg:w-1/3 xl:w-1/3 pt-4">
