e10ad2a480f9cb2b5700859344ae0280e43c2ec6
Slant Dev <build@amazeworx.com>
2024-02-29 08:32:51 +0700

    single job

--- template-parts/single/single-job.php
+++ template-parts/single/single-job.php
@@ -1,6 +1,6 @@
 <?php
 
-$enable_page_header = true;
+$enable_page_header = false;
 $text_color = '#000000';
 $text_background_color = 'rgba(242,107,73,0.93)';
 $background_image = get_the_post_thumbnail_url(get_the_ID(), 'full');
@@ -16,41 +16,50 @@ $text_bg_style = $text_background_color ? 'background-color: ' . $text_backgroun
 $enable_breadcrumbs = true;
 
 ?>
-<section class="bg-gradient-to-b from-brand-light-gray from-90% via-white via-90% to-white">
-  <div class="max-w-screen-5xl px-4 mx-auto 4xl:px-8">
-    <div class="aspect-w-16 aspect-h-5 rounded-3xl overflow-hidden">
-      <div class="bg-zinc-300 w-full h-full">
-        <?php if ($background_image) : ?>
-          <img class="object-cover w-full h-full <?php echo $bg_image_class ?>" src="<?php echo $background_image; ?>" alt="">
-        <?php endif; ?>
-        <div class="absolute top-0 left-0 w-[1300px] h-[1300px] rounded-full bg-brand-orange bg-opacity-80 -translate-y-1/4 -translate-x-1/4" style="<?php echo $text_bg_style ?>"></div>
-        <div class="absolute inset-0 flex items-center p-12">
-          <div class="container max-w-screen-xxl mx-auto">
-            <div class="max-w-lg font-montserrat" style="<?php echo $text_style ?>">
-              <?php if ($enable_breadcrumbs) : ?>
-                <?php
-                if (function_exists('yoast_breadcrumb')) {
-                  yoast_breadcrumb('<div class="breadcrumbs mb-10 text-white">', '</div>');
-                }
-                ?>
-              <?php endif; ?>
-              <?php if ($title) : ?>
-                <h2 class="text-4xl text-white font-bold mb-4"><?php echo $title ?></h2>
-              <?php endif; ?>
+<?php if ($enable_page_header) : ?>
+  <section class="bg-gradient-to-b from-brand-light-gray from-90% via-white via-90% to-white">
+    <div class="max-w-screen-5xl px-4 mx-auto 4xl:px-8">
+      <div class="aspect-w-16 aspect-h-5 rounded-3xl overflow-hidden">
+        <div class="bg-zinc-300 w-full h-full">
+          <?php if ($background_image) : ?>
+            <img class="object-cover w-full h-full <?php echo $bg_image_class ?>" src="<?php echo $background_image; ?>" alt="">
+          <?php endif; ?>
+          <div class="absolute top-0 left-0 w-[1300px] h-[1300px] rounded-full bg-brand-orange bg-opacity-80 -translate-y-1/4 -translate-x-1/4" style="<?php echo $text_bg_style ?>"></div>
+          <div class="absolute inset-0 flex items-center p-12">
+            <div class="container max-w-screen-xxl mx-auto">
+              <div class="max-w-lg font-montserrat" style="<?php echo $text_style ?>">
+                <?php if ($enable_breadcrumbs) : ?>
+                  <?php
+                  if (function_exists('yoast_breadcrumb')) {
+                    yoast_breadcrumb('<div class="breadcrumbs mb-10 text-white">', '</div>');
+                  }
+                  ?>
+                <?php endif; ?>
+                <?php if ($title) : ?>
+                  <h2 class="text-4xl text-white font-bold mb-4"><?php echo $title ?></h2>
+                <?php endif; ?>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</section>
+  </section>
+<?php endif; ?>
 
 <section class="bg-white">
   <div class="relative container max-w-screen-xxl mx-auto pt-12 lg:pt-20 xl:pt-20 pb-12 lg:pb-20 xl:pb-36">
     <div class="flex flex-wrap lg:flex-nowrap lg:gap-x-16 xl:gap-x-16 3xl:gap-x-24">
       <div class="w-full order-1 lg:w-2/3 xl:w-2/3 pt-4">
+        <?php if (!$enable_page_header && $enable_breadcrumbs) : ?>
+          <?php
+          if (function_exists('yoast_breadcrumb')) {
+            yoast_breadcrumb('<div class="breadcrumbs mb-10">', '</div>');
+          }
+          ?>
+        <?php endif; ?>
         <div class="not-prose">
-          <h2 class="mb-8 xl:mb-12 text-left text-3xl font-bold"><?php echo get_the_title() ?></h2>
+          <h2 class="mb-8 xl:mb-12 text-left text-3xl xl:text-4xl font-bold"><?php echo get_the_title() ?></h2>
         </div>
         <div class="prose max-w-none xl:prose-lg mr-auto text-left mb-6 xl:mb-8">
           <?php the_content() ?>
