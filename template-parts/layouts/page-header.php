18ef368de7a5b767d1bedb5dfaa8293b6898612f
Slant Dev <build@amazeworx.com>
2024-04-17 09:41:33 +0700

    page banner h1

--- template-parts/layouts/page-header.php
+++ template-parts/layouts/page-header.php
@@ -58,7 +58,7 @@ if ($enable_page_header) :
                   ?>
                 <?php endif; ?>
                 <?php if ($title) : ?>
-                  <h2 class="text-3xl xl:text-4xl 3xl:text-5xl font-bold mb-4"><?php echo $title ?></h2>
+                  <h1 class="text-3xl xl:text-4xl 3xl:text-5xl font-bold mb-4"><?php echo $title ?></h1>
                 <?php endif; ?>
                 <?php if ($description) : ?>
                   <div class="text-sm xl:text-lg 3xl:text-xl xl:leading-snug font-medium mt-4">
