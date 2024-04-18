17329deefdd6b9d8e868820557c119955c5207a1
Slant Dev <build@amazeworx.com>
2024-04-19 02:17:07 +0700

    homepage banner mobile

--- template-parts/sections/hero_slider.php
+++ template-parts/sections/hero_slider.php
@@ -22,9 +22,6 @@ if ($hero_slider) : ?>
           <div class="swiper-wrapper">
             <?php foreach ($hero_slider as $slide) : ?>
               <?php
-              $background = $slide['background'];
-              $slide_image = $background['background_image'];
-              $bg_overlay = $background['background_overlay_color'];
               $headline = $slide['headline'];
               $headline_text = $headline['headline'];
               $headline_color = $headline['headline_color'];
@@ -32,38 +29,65 @@ if ($hero_slider) : ?>
               if ($headline_color) {
                 $headline_style .= 'color: ' . $headline_color . ';';
               }
-              $description = $slide['description'];
-              $description_text = $description['description'];
-              $description_color = $description['description_color'];
+              $headline_html_tag = $headline['headline_html_tag'] ?? 'h2';
+              if ($headline_html_tag == 'default') {
+                $headline_html_tag = 'h2';
+              }
+              $description = $slide['description'] ?? '';
+              $description_text = $description['description'] ?? '';
+              $description_mobile = $description['description_mobile'] ?? '';
+              $description_color = $description['description_color'] ?? '';
               $description_style = '';
               if ($description_color) {
                 $description_style .= 'color: ' . $description_color . ';';
               }
-              $buttons = $slide['buttons'];
+              $buttons = $slide['buttons'] ?? '';
 
+              $background = $slide['background'] ?? '';
+              $slide_image = $background['background_image'] ?? '';
+              $slide_image_mobile = $background['background_image_mobile'] ?? '';
+              $bg_overlay = $background['background_overlay_color'] ?? '';
               $overlay_style = '';
               if ($bg_overlay) {
                 $overlay_style = 'background-color: ' . $bg_overlay;
               }
               ?>
               <div class="swiper-slide">
-                <div class="aspect-w-4 aspect-h-6 md:aspect-w-16 md:aspect-h-7">
-                  <?php if ($slide_image) : ?>
+                <div class="aspect-w-4 aspect-h-6 md:aspect-w-16 md:aspect-h-9 xl:aspect-w-16 xl:aspect-h-7">
+                  <?php if ($slide_image || $slide_image_mobile) : ?>
                     <div class="absolute inset-0">
                       <div class="absolute inset-0 mix-blend-multiply" style="<?php echo $overlay_style ?>"></div>
-                      <img src="<?php echo $slide_image['url'] ?>" alt="<?php echo $slide_image['alt'] ?>" class="object-cover h-full w-full opacity-100">
+                      <?php if ($slide_image_mobile) : ?>
+                        <img src="<?php echo $slide_image_mobile['url'] ?>" alt="<?php echo $slide_image_mobile['alt'] ?>" class="object-cover h-full w-full opacity-100 lg:hidden">
+                        <img src="<?php echo $slide_image['url'] ?>" alt="<?php echo $slide_image['alt'] ?>" class="object-cover h-full w-full opacity-100 hidden md:block">
+                      <?php else : ?>
+                        <img src="<?php echo $slide_image['url'] ?>" alt="<?php echo $slide_image['alt'] ?>" class="object-cover h-full w-full opacity-100">
+                      <?php endif ?>
                     </div>
                   <?php endif; ?>
-                  <div class="absolute inset-0 flex items-end md:items-center">
-                    <div class="container max-w-screen-3xl mx-auto pb-8 md:pb-0">
-                      <div class="w-full md:w-1/2 font-montserrat">
-                        <div class="w-2/3 md:w-full">
-                          <?php if ($headline_text) : ?>
-                            <h2 class="hero-headline text-2xl lg:text-[64px] lg:leading-tight text-brand-black font-bold mb-4" style="<?php echo $headline_style ?>"><?php echo $headline_text; ?></h2>
-                          <?php endif; ?>
+                  <div class="absolute inset-0 flex items-end lg:items-center">
+                    <div class="container max-w-screen-3xl mx-auto pb-8 md:pb-4 lg:pb-0">
+                      <div class="w-full md:w-3/4 lg:w-1/2 font-montserrat">
+                        <div class="w-full pr-6 md:pr-0">
+                          <?php
+                          if ($headline_text) {
+                            echo '<div class="not-prose">';
+                            echo '<' . $headline_html_tag;
+                            echo ' class="hero-headline text-[28px] leading-tight md:text-4xl md:leading-tight xl:text-[64px] xl:leading-tight text-brand-black font-bold mb-4"';
+                            echo ' style="' . $headline_style . '">';
+                            echo $headline_text;
+                            echo '</' . $headline_html_tag . '>';
+                            echo '</div>';
+                          }
+                          ?>
                           <?php if ($description_text) : ?>
-                            <div class="hero-description text-base lg:text-2xl font-medium mt-4" style="<?php echo $description_style ?>">
-                              <?php echo $description_text; ?>
+                            <div class="hero-description text-sm md:text-base xl:text-2xl font-medium mt-4" style="<?php echo $description_style ?>">
+                              <?php if ($description_mobile) : ?>
+                                <div class="md:hidden"><?php echo $description_mobile; ?></div>
+                                <div class="hidden md:block"><?php echo $description_text; ?></div>
+                              <?php else : ?>
+                                <div><?php echo $description_text; ?></div>
+                              <?php endif ?>
                             </div>
                           <?php endif; ?>
                         </div>
@@ -75,7 +99,7 @@ if ($hero_slider) : ?>
               </div>
             <?php endforeach; ?>
           </div>
-          <div class="absolute left-0 bottom-3 md:bottom-8 right-0">
+          <div class="absolute left-0 bottom-3 md:bottom-2 lg:bottom-8 right-0">
             <div class="container max-w-screen-3xl mx-auto">
               <div class="swiper-pagination text-left relative" style="--swiper-pagination-bullet-size:12px;--swiper-pagination-bullet-inactive-color:#fff;--swiper-pagination-bullet-horizontal-gap:6px;--swiper-theme-color:#45C2BF;--swiper-pagination-bullet-inactive-opacity:1;"></div>
             </div>
