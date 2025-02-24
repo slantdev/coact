10beed7259ca4c4baac4eeb0c059123d0ffc61b0
Slant Dev <build@amazeworx.com>
2025-02-24 10:00:31 +0700

    remove reciteme button

--- footer.php
+++ footer.php
@@ -12,11 +12,6 @@
 
 <?php wp_footer(); ?>
 
-<div class="fixed z-50 right-2.5 bottom-3 md:bottom-[2px] rounded-full shadow-[0_0_8px_0_rgba(0,0,0,0.5)] text-blue-600">
-  <a href="#reciteme" class="text-blue-600">
-    <?php echo coact_icon(array('icon' => 'accesibility', 'group' => 'utilities', 'size' => '48', 'class' => 'w-[46px] h-[46px] md:w-[44px] md:h-[44px]')); ?>
-  </a>
-</div>
 </body>
 
 </html>
