19f19252d0f1746d3d676e6c72fc8e94691e7314
Slant Dev <build@amazeworx.com>
2025-10-13 11:27:32 +0700

    fix google maps

--- assets/js/service-locator.js
+++ assets/js/service-locator.js
@@ -294,11 +294,13 @@
                 service_type_tags += '<span class="inline-block text-[11px] px-3 py-0.5 rounded-md bg-white border border-brand-sea text-brand-sea">' + tag[0] + "</span>";
               }
               var list_contact_numbers = "";
-              contact_numbers.forEach(function(element) {
-                var tel = element.phone_number;
-                tel = tel.replace(/\s+/g, "");
-                list_contact_numbers += '<div class="mb-1">' + element.phone_label + ':&nbsp;<a href="tel:' + tel + '" class="underline font-medium hover:no-underline">' + element.phone_number + "</a></div>";
-              });
+              if (contact_numbers) {
+                contact_numbers.forEach(function(element) {
+                  var tel = element.phone_number;
+                  tel = tel.replace(/\s+/g, "");
+                  list_contact_numbers += '<div class="mb-1">' + element.phone_label + ':&nbsp;<a href="tel:' + tel + '" class="underline font-medium hover:no-underline">' + element.phone_number + "</a></div>";
+                });
+              }
               var activeInfoWindow;
               var infowindow = new google.maps.InfoWindow();
               google.maps.event.addListener(marker, "click", function(evt) {
@@ -345,11 +347,13 @@
               service_type_tags += '<span class="inline-block text-[11px] px-3 py-0.5 rounded-md bg-white border border-brand-sea text-brand-sea">' + tag[0] + "</span>";
             }
             var list_contact_numbers = "";
-            contact_numbers.forEach(function(element) {
-              var tel = element.phone_number;
-              tel = tel.replace(/\s+/g, "");
-              list_contact_numbers += '<div class="mb-1">' + element.phone_label + ':&nbsp;<a href="tel:' + tel + '" class="underline font-medium hover:no-underline">' + element.phone_number + "</a></div>";
-            });
+            if (contact_numbers) {
+              contact_numbers.forEach(function(element) {
+                var tel = element.phone_number;
+                tel = tel.replace(/\s+/g, "");
+                list_contact_numbers += '<div class="mb-1">' + element.phone_label + ':&nbsp;<a href="tel:' + tel + '" class="underline font-medium hover:no-underline">' + element.phone_number + "</a></div>";
+              });
+            }
             var activeInfoWindow;
             var infowindow = new google.maps.InfoWindow();
             google.maps.event.addListener(marker, "click", function(evt) {
@@ -739,18 +743,22 @@
         }
         var contact_numbers = data.acf.contact_numbers;
         var list_contact_numbers = "";
-        contact_numbers.forEach(function(element) {
-          var tel = element.phone_number;
-          tel = tel.replace(/\s+/g, "");
-          list_contact_numbers += '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="injected-svg" data-src="/wp-content/themes/coact/assets/images/service-locator/common-phone.svg"><path class="svg_inherit" d="M6.62 10.79a15.15 15.15 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.02-.24c1.12.37 2.33.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57a1 1 0 0 1-.25 1.02l-2.2 2.2z"></path></svg></div></div></span><a href="tel:' + tel + '" class="phantom-phone-number">' + element.phone_label + ":&nbsp;" + element.phone_number + "</a></div>";
-        });
+        if (contact_numbers) {
+          contact_numbers.forEach(function(element) {
+            var tel = element.phone_number;
+            tel = tel.replace(/\s+/g, "");
+            list_contact_numbers += '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="injected-svg" data-src="/wp-content/themes/coact/assets/images/service-locator/common-phone.svg"><path class="svg_inherit" d="M6.62 10.79a15.15 15.15 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.02-.24c1.12.37 2.33.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57a1 1 0 0 1-.25 1.02l-2.2 2.2z"></path></svg></div></div></span><a href="tel:' + tel + '" class="phantom-phone-number">' + element.phone_label + ":&nbsp;" + element.phone_number + "</a></div>";
+          });
+        }
         var address_data = data.acf.location.address;
         var description_data = data.acf.description;
         var checkmark_data = data.acf.checkmark_list;
         var checkmark_list = "";
-        checkmark_data.forEach(function(element) {
-          checkmark_list += '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="injected-svg" data-src="/wp-content/themes/coact/assets/images/service-locator/common-tick.svg"><title>icon / tick</title><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path fill="#45C2BF" fill-rule="nonzero" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z"></path></g></svg></div></div></span><span>' + element.point + "</span></div>";
-        });
+        if (checkmark_data) {
+          checkmark_data.forEach(function(element) {
+            checkmark_list += '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="injected-svg" data-src="/wp-content/themes/coact/assets/images/service-locator/common-tick.svg"><title>icon / tick</title><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path fill="#45C2BF" fill-rule="nonzero" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z"></path></g></svg></div></div></span><span>' + element.point + "</span></div>";
+          });
+        }
         var link_data = data.link;
         var details_body = "";
         details_body += '<div class="service_locator-details_title details_block">';
--- resources/js/service-locator.js
+++ resources/js/service-locator.js
@@ -471,22 +471,24 @@ jQuery(function ($) {
             }
 
             var list_contact_numbers = "";
-            contact_numbers.forEach(function (element) {
-              //console.log(element.phone_label);
-              var tel = element.phone_number;
-              tel = tel.replace(/\s+/g, "");
+            if (contact_numbers) {
+              contact_numbers.forEach(function (element) {
+                //console.log(element.phone_label);
+                var tel = element.phone_number;
+                tel = tel.replace(/\s+/g, "");
 
-              list_contact_numbers +=
-                '<div class="mb-1">' +
-                element.phone_label +
-                ":&nbsp;" +
-                '<a href="tel:' +
-                tel +
-                '" class="underline font-medium hover:no-underline">' +
-                element.phone_number +
-                "</a>" +
-                "</div>";
-            });
+                list_contact_numbers +=
+                  '<div class="mb-1">' +
+                  element.phone_label +
+                  ":&nbsp;" +
+                  '<a href="tel:' +
+                  tel +
+                  '" class="underline font-medium hover:no-underline">' +
+                  element.phone_number +
+                  "</a>" +
+                  "</div>";
+              });
+            }
 
             var activeInfoWindow;
             var infowindow = new google.maps.InfoWindow();
@@ -581,22 +583,24 @@ jQuery(function ($) {
           }
 
           var list_contact_numbers = "";
-          contact_numbers.forEach(function (element) {
-            //console.log(element.phone_label);
-            var tel = element.phone_number;
-            tel = tel.replace(/\s+/g, "");
+          if (contact_numbers) {
+            contact_numbers.forEach(function (element) {
+              //console.log(element.phone_label);
+              var tel = element.phone_number;
+              tel = tel.replace(/\s+/g, "");
 
-            list_contact_numbers +=
-              '<div class="mb-1">' +
-              element.phone_label +
-              ":&nbsp;" +
-              '<a href="tel:' +
-              tel +
-              '" class="underline font-medium hover:no-underline">' +
-              element.phone_number +
-              "</a>" +
-              "</div>";
-          });
+              list_contact_numbers +=
+                '<div class="mb-1">' +
+                element.phone_label +
+                ":&nbsp;" +
+                '<a href="tel:' +
+                tel +
+                '" class="underline font-medium hover:no-underline">' +
+                element.phone_number +
+                "</a>" +
+                "</div>";
+            });
+          }
 
           var activeInfoWindow;
           var infowindow = new google.maps.InfoWindow();
@@ -1234,37 +1238,41 @@ jQuery(function ($) {
       }
       var contact_numbers = data.acf.contact_numbers;
       var list_contact_numbers = "";
-      contact_numbers.forEach(function (element) {
-        //console.log(element.phone_label);
-        var tel = element.phone_number;
-        tel = tel.replace(/\s+/g, "");
+      if (contact_numbers) {
+        contact_numbers.forEach(function (element) {
+          //console.log(element.phone_label);
+          var tel = element.phone_number;
+          tel = tel.replace(/\s+/g, "");
 
-        list_contact_numbers +=
-          '<div class="service_locator-proofPoint">' +
-          '<span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="injected-svg" data-src="' +
-          '/wp-content/themes/coact/assets/images/service-locator/common-phone.svg"><path class="svg_inherit" d="M6.62 10.79a15.15 15.15 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.02-.24c1.12.37 2.33.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57a1 1 0 0 1-.25 1.02l-2.2 2.2z"></path></svg></div></div></span>' +
-          '<a href="tel:' +
-          tel +
-          '" class="phantom-phone-number">' +
-          element.phone_label +
-          ":&nbsp;" +
-          element.phone_number +
-          "</a>" +
-          "</div>";
-      });
+          list_contact_numbers +=
+            '<div class="service_locator-proofPoint">' +
+            '<span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" class="injected-svg" data-src="' +
+            '/wp-content/themes/coact/assets/images/service-locator/common-phone.svg"><path class="svg_inherit" d="M6.62 10.79a15.15 15.15 0 0 0 6.59 6.59l2.2-2.2a1 1 0 0 1 1.02-.24c1.12.37 2.33.57 3.57.57a1 1 0 0 1 1 1V20a1 1 0 0 1-1 1A17 17 0 0 1 3 4a1 1 0 0 1 1-1h3.5a1 1 0 0 1 1 1c0 1.25.2 2.45.57 3.57a1 1 0 0 1-.25 1.02l-2.2 2.2z"></path></svg></div></div></span>' +
+            '<a href="tel:' +
+            tel +
+            '" class="phantom-phone-number">' +
+            element.phone_label +
+            ":&nbsp;" +
+            element.phone_number +
+            "</a>" +
+            "</div>";
+        });
+      }
 
       var address_data = data.acf.location.address;
       var description_data = data.acf.description;
 
       var checkmark_data = data.acf.checkmark_list;
       var checkmark_list = "";
-      checkmark_data.forEach(function (element) {
-        checkmark_list +=
-          '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="injected-svg" data-src="' +
-          '/wp-content/themes/coact/assets/images/service-locator/common-tick.svg"><title>icon / tick</title><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path fill="#45C2BF" fill-rule="nonzero" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z"></path></g></svg></div></div></span><span>' +
-          element.point +
-          "</span></div>";
-      });
+      if (checkmark_data) {
+        checkmark_data.forEach(function (element) {
+          checkmark_list +=
+            '<div class="service_locator-proofPoint"><span class="svg_icon"><div><div><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" class="injected-svg" data-src="' +
+            '/wp-content/themes/coact/assets/images/service-locator/common-tick.svg"><title>icon / tick</title><g fill="none" fill-rule="evenodd"><path d="M0 0h24v24H0z"></path><path fill="#45C2BF" fill-rule="nonzero" d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4z"></path></g></svg></div></div></span><span>' +
+            element.point +
+            "</span></div>";
+        });
+      }
 
       var link_data = data.link;
 
